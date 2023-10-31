0c46024d2ba5ae400612e41aeeb6cdc1a971fe3f
anthonyg876 <anthonygravier2@gmail.com>
2023-10-31 15:18:10 -0600

    restructured the payment model to display the information given below

--- models/payment.go
+++ models/payment.go
@@ -1,13 +1,15 @@
 package models
 
 type Payment struct {
-	OrderTotal string `json:"orderTotal"`
-	Products   []struct {
-		ProductId    string `json:"productId"`
-		Quantity     int    `json:"quantity"`
-		Price        int64    `json:"price"`
-		BoughtInBulk bool   `json:"boughtInBulk"`
-	} `json:"products"`
-	CustomerId string    `json:"customerId"`
-	ReaderId   string `json:"readerId"`
+	OrderTotal string         `json:"orderTotal"`
+	Products   []ProductOrder `json:"products"`
+	CustomerId string         `json:"customerId"`
+	ReaderId   string         `json:"readerId"`
+}
+
+type ProductOrder struct {
+	ProductId    string `json:"productId"`
+	Quantity     int    `json:"quantity"`
+	Price        int64  `json:"price"`
+	BoughtInBulk bool   `json:"boughtInBulk"`
 }
